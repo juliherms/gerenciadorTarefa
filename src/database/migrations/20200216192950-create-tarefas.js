b9d49a0eb9d5fb8cc62fc4019a8fc4669032576b
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-02-16 19:31:48 -0300

    commit do migrate de tarefas com sequelize

--- src/database/migrations/20200216192950-create-tarefas.js
+++ src/database/migrations/20200216192950-create-tarefas.js
@@ -1,11 +1,35 @@
 'use strict';
 
+/**
+ * Criar uma tabela tarefa com os seguintes campos:
+ * "descrição", a "data de criação" e um "status" que pode variar entre "pendente", "andamento" e "concluida"
+ */
 module.exports = {
   up: (queryInterface, Sequelize) => {
       return queryInterface.createTable('tarefas', {
-         id: Sequelize.INTEGER 
+         id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true 
+        },
+        descricao: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        status: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        created_at : {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        updated_at:  {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
       });
-
   },
 
   down: (queryInterface, Sequelize) => {
